157edc0b256d4f38e464dc43694e3f463cb5bf84
Oyedeji Oyewole <57059138+oyedejioyewole@users.noreply.github.com>
2026-09-09 04:27:47 +0100

    Improve scripts/docker-entrypoint.sh

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
+set -eu
 
 WS_PORT_START=${CAMOUFOX_WS_PORT_START:-9222}
-WS_POOL_SIZE=${CAMOUFOX_POOL_SIZE:-8}
+WS_PORT_END=$((${CAMOUFOX_POOL_SIZE:-8} + WS_PORT_START - 1))
+
+log() {
+    echo "Forwarding TCP4 connections on port $1 to TCP6 ::1 on port $1"
+}
 
 # Start socat listeners in the background
 if [ "$CAMOUFOX_MODE" = "pool" ]; then
-    for port in $(seq $WS_PORT_START $((WS_PORT_START + WS_POOL_SIZE))); do
+    for port in $(seq $WS_PORT_START $WS_PORT_END); do
         socat TCP4-LISTEN:$port,bind=0.0.0.0,fork,reuseaddr TCP6:[::1]:$port &
-        echo "Starting socat listener on port $port"
+        log $port
+
+        if [ "$port" -eq "$WS_PORT_END" ]; then
+            echo ""
+        fi
     done
 else
     socat TCP4-LISTEN:$WS_PORT_START,bind=0.0.0.0,fork,reuseaddr TCP6:[::1]:$WS_PORT_START &
-    echo "Starting socat listener on port $WS_PORT_START"
+    log $WS_PORT_START
 fi
 
 # Replace the shell with the Python server, so it becomes the main process
